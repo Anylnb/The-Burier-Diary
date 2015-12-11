667ecdda3d3121cc23f712b4511bf1100f020f15
Anylnb <acehuang27@gmail.com>
2015-12-11 12:53:52 -0600

    My entry graph, diarybook update

--- js/graph.js
+++ js/graph.js
@@ -38,22 +38,22 @@ $(function () {
                 name: 'Brands',
                 colorByPoint: true,
                 data: [{
-                    name: 'Microsoft Internet Explorer',
+                    name: 'Happy',
                     y: 56.33
                 }, {
-                    name: 'Chrome',
+                    name: 'Normal',
                     y: 24.03,
                 }, {
-                    name: 'Firefox',
+                    name: 'Sad',
                     y: 10.38
                 }, {
-                    name: 'Safari',
+                    name: 'Depressed',
                     y: 4.77
                 }, {
-                    name: 'Opera',
+                    name: 'Super Happy',
                     y: 0.91
                 }, {
-                    name: 'Proprietary or Undetectable',
+                    name: 'Feel proud of yourself',
                     y: 0.2
                 }]
             }]
@@ -79,12 +79,23 @@ $(function () {
             x: -20
         },
         xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'
+            categories: ['Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
                 ]
         },
         yAxis: {
             title: {
-                text: 'Temperature (°C)'
+                text: 'Mood (Times)'
             },
             plotLines: [{
                 value: 0,
@@ -93,7 +104,7 @@ $(function () {
             }]
         },
         tooltip: {
-            valueSuffix: '°C'
+            valueSuffix: ' times'
         },
         legend: {
             layout: 'vertical',
@@ -102,16 +113,16 @@ $(function () {
             borderWidth: 0
         },
         series: [{
-            name: 'Tokyo',
+            name: 'Happy',
             data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
         }, {
-            name: 'New York',
+            name: 'Sad',
             data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
         }, {
-            name: 'Berlin',
+            name: 'Depressed',
             data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
         }, {
-            name: 'London',
+            name: 'Normal',
             data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
         }]
     });
@@ -142,20 +153,26 @@ $(function () {
                 'Mar',
                 'Apr',
                 'May',
-                'Jun'
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
             ],
             crosshair: true
         },
         yAxis: {
             min: 0,
             title: {
-                text: 'Rainfall (mm)'
+                text: 'Mood (Times)'
             }
         },
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                '<td style="padding:0"><b>{point.y:.1f} times</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
@@ -167,19 +184,19 @@ $(function () {
             }
         },
         series: [{
-            name: 'Tokyo',
+            name: 'Happy',
             data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
 
         }, {
-            name: 'New York',
+            name: 'Sad',
             data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
 
         }, {
-            name: 'London',
+            name: 'Depressed',
             data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
 
         }, {
-            name: 'Berlin',
+            name: 'Normal',
             data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
 
         }]
